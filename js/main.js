b484eca821fe5429700f4442927c590c59ac9932
a6salyamov <122038238+a6salyamov@users.noreply.github.com>
2023-01-28 16:30:40 +0500

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,3 +1,42 @@
+const value = [{
+  value: '1',
+  label: '(999) <img src="images/contact-country.svg"/>',
+  id: 1
+},
+
+{
+  value: '2',
+  label: '(888) <img src="images/contact-country.svg"/>',
+  id: 2
+},
+
+{
+  value: '3',
+  label: '(777) <img src="images/contact-country.svg"/>',
+  id: 3
+},
+
+
+{
+  value: '4',
+  label: '(666) <img src="images/contact-country.svg"/>',
+  id: 4
+},
+
+]
+
+
+const defaultSelect = () => {
+  const element = document.querySelector('.contact__choices');
+  const choices = new Choices(element, {
+    searchEnabled: false,
+    allowHTML: true,
+    choices: value,
+  });
+};
+
+defaultSelect();
+
 $(function(){
   $(".country__content").not(":first").hide()
   $(".country__list .country__list-item").on("click", function(){
@@ -29,9 +68,12 @@ $(function(){
     nextArrow: '<span class="next_arrow"><div class="programm__accordion-ellipse"><span></span><span></span></div></span>',
   });
 
-
-
-
+  $('.burger, .overlay').on('click', function(e) {
+    e.preventDefault()
+    $('.nav').toggleClass('nav--open')
+    $('.burger').toggleClass('burger--close')
+    $('.overlay').toggleClass('overlay--show')
+  } )
 
 })
 
@@ -47,5 +89,7 @@ document.querySelectorAll('.programm__accordion-title').forEach((el) => {
     }
   })
 })
+
+
 
 
